5d979ce0928a687531c518bacfb3620b2e43fc3b
Woomin <worejj@gmail.com>
2021-10-18 17:05:38 +0900

    "오타 수정"

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const path = require('path');
 
-const prot = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/build")));
